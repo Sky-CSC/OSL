8c80723328d7e02d3eef9cee5efbb7c6f10e1187
Sky <fabien-perot@orange.fr>
2022-05-11 00:34:30 +0200

    RiotApp

---
+++ OSR-Client/RiotApp/LaunchChecker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using OSR_Client.Configuration;
+
+namespace OSR_Client.RiotApp
+{
+    /// <summary>
+    /// Check all programm or what is launch in relation of RIOT
+    /// </summary>
+    public class LaunchChecker
+    {
+        private static OSRLogger _logger = new OSRLogger("LaunchChecker");
+        /// <summary>
+        /// Check if League Of Legends client is launch
+        /// </summary>
+        /// <returns></returns>
+        public static void LoLLauncherCheck()
+        {
+            Process[] localAll = ProcessInfo.NameProcessInfo(Config.LeagueClientProcess);
+            if (localAll.Length > 0)
+            {
+                try
+                {
+                    Config.LeagueClientFullFilePath = localAll[0].MainModule.FileName;
+                    string[] splitPath = Config.LeagueClientFullFilePath.Split(localAll[0].MainModule.ModuleName);
+                    Config.LeagueClientFilePath = splitPath[0];
+                    _logger.log(LoggingLevel.INFO, "LaunchChecker()", "Riot Launcher Path : " + Config.LeagueClientFilePath);
+                }
+                catch (Exception)
+                {
+                    _logger.log(LoggingLevel.ERROR, "LaunchChecker()", "Riot Launcher Path not found");
+                }
+            }
+            else
+            {
+                Thread.Sleep(5000);
+                LoLLauncherCheck();
+            }
+        }
+    }
+}
---
+++ OSR-Client/RiotApp/ProcessInfo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace OSR_Client.RiotApp
+{
+    public class ProcessInfo
+    {
+        private static OSRLogger _logger = new OSRLogger("ProcessInfo");
+        public static Process[] NameProcessInfo(string nameProcess)
+        {
+            try
+            {
+                Process[] localAll = Process.GetProcessesByName(nameProcess);
+                if (localAll.Length > 0)
+                {
+                    _logger.log(LoggingLevel.INFO, "NameProcessInfo()", $"Found processes info with name {nameProcess}");
+                    return localAll;
+                }
+                else {
+                    _logger.log(LoggingLevel.ERROR, "NameProcessInfo()", $"Could not find processes info with name {nameProcess}");
+                    return Array.Empty<Process>();
+                }
+            }
+            catch
+            {
+                _logger.log(LoggingLevel.ERROR, "NameProcessInfo()", $"Could not find processes info with name {nameProcess}");
+                return Array.Empty<Process>();
+            }
+        }
+
+        public static Process IdProcessInfo(int idProcess)
+        {
+            try
+            {
+                Process localAll = Process.GetProcessById(idProcess);
+                _logger.log(LoggingLevel.INFO, "NameProcessInfo()", $"Found processes ID with name {idProcess}");
+                return localAll;
+            }
+            catch
+            {
+                _logger.log(LoggingLevel.ERROR, "NameProcessInfo()", $"Could not find processes ID with name {idProcess}");
+                return null;
+            }
+        }
+    }
+}
