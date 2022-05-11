573587b293446fdd5d1807be20980346b98a0056
Sky <fabien-perot@orange.fr>
2022-05-11 17:57:48 +0200

    Update Config.cs

--- OSR-Client/Configuration/Config.cs
+++ OSR-Client/Configuration/Config.cs
@@ -14,16 +14,25 @@ namespace OSR_Client.Configuration
 
         public static string ip = "localhost";
         public static int port = 456789;
-        public static string LeagueClientProcess = "LeagueClient";
-        public static string LeagueClientFullFilePath = "";
-        public static string LeagueClientFilePath = "";
-        
+        public static string leagueClientProcess = "LeagueClient";
+        public static string leagueClientFullFilePath = "";
+        public static string leagueClientPath = "";
+        public static string lockFilePath = "";
+        public static int lockFileProcessId;
+        public static int lockFilePort;
+        public static string lockFilePassword = "";
+        public static string localIpHttps = "https://127.0.0.1";
+        public static string localIpHttp = "127.0.0.1";
+        public static string loginRiot = "riot";
+        public static string portRiot = "2999";
+        public static string GameClientApiLocalHost = "";
+        public static string GameClientApiPassword = "";
+
         /// <summary>
         /// Loads the config from the config file.
         /// </summary>
         public static void LoadConfigHost()
         {
-
             string filePath = "./" + "Config" + "/" + "configHost.json";
             dynamic configHost = JsonConvert.DeserializeObject(FileManagerLocal.ReadInFile(filePath));
             try
@@ -35,6 +44,22 @@ namespace OSR_Client.Configuration
             catch (Exception e)
             {
                 _logger.log(LoggingLevel.ERROR, "LoadConfigHost()", e.Message);
+            }
+        }
+
+        public static bool SetHostPassGameClientApi()
+        {
+            try
+            {
+                GameClientApiLocalHost = Config.localIpHttp + ":" + Config.lockFilePort;
+                GameClientApiPassword = "Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes($"{Config.loginRiot}:{Config.lockFilePassword}"));
+                _logger.log(LoggingLevel.INFO, "HostPassGameClientApi()", "Host and password game client api set");
+                return true;
+            }
+            catch
+            {
+                _logger.log(LoggingLevel.ERROR, "HostPassGameClientApi()", "Error host and password to game client api");
+                return false;
             }
         }
     }
