3b22e656ea90760d6aab08cf581c198c37dfcd8f
Sky <fabien-perot@orange.fr>
2022-05-11 17:57:42 +0200

    RiotApp

--- OSR-Client/RiotApp/LaunchChecker.cs
+++ OSR-Client/RiotApp/LaunchChecker.cs
@@ -19,27 +19,30 @@ namespace OSR_Client.RiotApp
         /// Check if League Of Legends client is launch
         /// </summary>
         /// <returns></returns>
-        public static void LoLLauncherCheck()
+        public static bool LoLLauncherCheck()
         {
-            Process[] localAll = ProcessInfo.NameProcessInfo(Config.LeagueClientProcess);
+            Process[] localAll = ProcessInfo.NameProcessInfo(Config.leagueClientProcess);
             if (localAll.Length > 0)
             {
                 try
                 {
-                    Config.LeagueClientFullFilePath = localAll[0].MainModule.FileName;
-                    string[] splitPath = Config.LeagueClientFullFilePath.Split(localAll[0].MainModule.ModuleName);
-                    Config.LeagueClientFilePath = splitPath[0];
-                    _logger.log(LoggingLevel.INFO, "LaunchChecker()", "Riot Launcher Path : " + Config.LeagueClientFilePath);
+                    Config.leagueClientFullFilePath = localAll[0].MainModule.FileName;
+                    string[] splitPath = Config.leagueClientFullFilePath.Split(localAll[0].MainModule.ModuleName);
+                    Config.leagueClientPath = splitPath[0];
+                    _logger.log(LoggingLevel.INFO, "LaunchChecker()", "Riot Launcher Path : " + Config.leagueClientPath);
+                    return true;
                 }
                 catch (Exception)
                 {
                     _logger.log(LoggingLevel.ERROR, "LaunchChecker()", "Riot Launcher Path not found");
+                    return false;
                 }
             }
             else
             {
+                _logger.log(LoggingLevel.ERROR, "LaunchChecker()", "Riot Launcher not found");
                 Thread.Sleep(5000);
-                LoLLauncherCheck();
+                return LoLLauncherCheck();
             }
         }
     }
