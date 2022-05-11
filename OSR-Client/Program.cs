79c98a160a16ec878c5eec7a976595fd72b12bf8
Sky <fabien-perot@orange.fr>
2022-05-11 17:57:57 +0200

    Update Program.cs

--- OSR-Client/Program.cs
+++ OSR-Client/Program.cs
@@ -1,6 +1,8 @@
 ﻿using OSR_Client;
 using OSR_Client.Configuration;
 using OSR_Client.RiotApp;
+using OSR_Client.RiotApp.API;
+using OSR_Client.RiotApp.API.LCU;
 
 Console.WriteLine("");
 OSRLogger logger = new("Program");
@@ -11,5 +13,25 @@ Console.WriteLine("# Hello and welcome to OSR manager #");
 Console.WriteLine("####################################\n");
 Console.ResetColor();
 
-Config.LoadConfigHost();
-LaunchChecker.LoLLauncherCheck();
+//Config.LoadConfigHost();
+
+//Initialisation for acces att the champion selection or a game
+if (LaunchChecker.LoLLauncherCheck())
+{
+    if (FilesRiotApp.LockFileCheck())
+    {
+        if (Config.SetHostPassGameClientApi())
+        {
+            string gameFlowPhase = ApiRequest.RequestGameClientAPI(UrlRequest.lolgameflowv1gameflowphase);
+            LcuApi.GameFlowPhaseCheck(gameFlowPhase);
+        }
+    }
+}
+
+//See what phase is of the game
+//if (ApiRequest.RequestGameClientAPI(LcuApi.lolgameflowv1session))
+//while (true)
+//{
+//    logger.log(LoggingLevel.INFO, "RequestGameClientApi()", ApiRequest.RequestGameClientAPI(UrlRequest.lolgameflowv1gameflowphase));
+//    Thread.Sleep(5000);
+//}
