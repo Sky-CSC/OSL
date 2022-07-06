3f2ff5f0d3d2fd448af0cff67ada394267b57a3d
Sky <fabien-perot@orange.fr>
2022-07-06 11:06:19 +0200

    ChampSelect Client
    Modification of json /session recive to add to it summoners names

--- OSL-Client/RiotApp/DataProcessing/ChampSelectProcess.cs
+++ OSL-Client/RiotApp/DataProcessing/ChampSelectProcess.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OSL_Client.FileManager;
 using OSL_Client.RiotApp.API;
 using OSL_Client.RiotApp.API.LCU;
 
@@ -15,19 +17,107 @@ namespace OSL_Client.RiotApp.DataProcessing
         /// <summary>
         /// Recovery of information on the champion selection dans send it to the API
         /// </summary>
+        static private System.Random random = new System.Random(DateTime.Now.Millisecond);
+
         public static void InChampSelect()
         {
-            //All information necessary for display pick and ban overlay
-            string champSelectContent = ApiRequest.RequestGameClientAPI(UrlRequest.lolchampselectv1session); 
-            if (champSelectContent == null)
+            NewGameProcess.IdGame = random.NextInt64(); //Possible server give IdGame
+            //Send to server json
+            var champSelect = new ChampSelect
             {
-                _logger.log(LoggingLevel.ERROR, "ManageChampionSelect()", "ChampSelectContent is null");
-            }
-            else
+                IdGame = NewGameProcess.IdGame,
+                Info = "ChampSelect",
+                Status = "Running",
+                Date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")
+            };
+            string InChampSelectSend = JsonConvert.SerializeObject(champSelect); //send to server information
+
+            //All information necessary for display pick and ban overlay
+            string champSelectContentPrevious = "";
+            string champSelectContent;
+            int i = 0;
+            while ((champSelectContent = ApiRequest.RequestGameClientAPI(UrlRequest.lolchampselectv1session)) != null)
             {
                 _logger.log(LoggingLevel.INFO, "ManageChampionSelect()", $"ChampSelectContent is {champSelectContent}");
-                //send post or put att server
+                if (!champSelectContent.Equals(champSelectContentPrevious))
+                {
+                    champSelectContentPrevious = champSelectContent;
+
+                    try
+                    {
+                        dynamic champSelectContentJson = JsonConvert.DeserializeObject(champSelectContent);
+                        foreach (var myTeamInfo in champSelectContentJson.myTeam)
+                        {
+                            string nameSummoner = "Bot";
+                            if (myTeamInfo.summonerId != 0)
+                            {
+                                string requestSummonerName = UrlRequest.lolsummonerv1summonersid + myTeamInfo.summonerId;
+                                string summonerContent = ApiRequest.RequestGameClientAPI(requestSummonerName);
+                                dynamic info = JsonConvert.DeserializeObject(summonerContent);
+                                nameSummoner = info.displayName;
+                                myTeamInfo.Property("summonerId").AddAfterSelf(new JProperty("summonerName", nameSummoner));
+                            }
+                            else
+                            {
+                                myTeamInfo.Property("summonerId").AddAfterSelf(new JProperty("summonerName", nameSummoner));
+                            }
+                        }
+
+                        foreach (var theirTeamInfo in champSelectContentJson.theirTeam)
+                        {
+                            string nameSummoner = "Bot";
+                            if (theirTeamInfo.summonerId != 0)
+                            {
+                                string requestSummonerName = UrlRequest.lolsummonerv1summonersid + theirTeamInfo.summonerId;
+                                string summonerContent = ApiRequest.RequestGameClientAPI(requestSummonerName);
+                                dynamic info = JsonConvert.DeserializeObject(summonerContent);
+                                nameSummoner = info.displayName;
+                                theirTeamInfo.Property("summonerId").AddAfterSelf(new JProperty("summonerName", nameSummoner));
+                            }
+                            else
+                            {
+                                theirTeamInfo.Property("summonerId").AddAfterSelf(new JProperty("summonerName", nameSummoner));
+                            }
+                        }
+
+                        //foreach (var myTeamInfo in champSelectContentJson)
+                        //{
+                        //    Console.WriteLine(myTeamInfo);
+                        //}
+
+                        champSelectContent = JsonConvert.SerializeObject(champSelectContentJson);
+
+                    }
+                    catch (Exception e)
+                    {
+                        _logger.log(LoggingLevel.ERROR, "InChampSelect()", $"Error Name Summoners {e.Message}");
+                    }
+
+                    FileManagerLocal.RewrittenFile($"E:/Overlay-found-riot/ChampSelectAll/champSelectAll{i}.json", champSelectContent);
+                    _logger.log(LoggingLevel.INFO, "ManageChampionSelect()", "Send ChampSelectContent");
+                    i++;
+                    //Send to Server ChampSelectContent
+                }
+                else
+                {
+                    _logger.log(LoggingLevel.WARN, "ManageChampionSelect()", "No modification of ChampSelectContent");
+                }
+                Thread.Sleep(1000);
             }
+            _logger.log(LoggingLevel.WARN, "ManageChampionSelect()", "End of ChampSelect");
+        }
+
+        private class summoner
+        {
+            public string NameSummoner { get; set; }
+        }
+
+        private class ChampSelect
+        {
+            public Int64 IdGame { get; set; }
+            public string? Info { get; set; }
+            public string? Status { get; set; }
+            public string? Date { get; set; }
         }
     }
 }
