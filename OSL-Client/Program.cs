5ea1bf2a6902512cf04d23078d9de730aa96fbaf
Sky <fabien-perot@orange.fr>
2023-01-15 15:38:00 +0100

    OSL-Client
    Update Program

--- OSL-Client/Program.cs
+++ OSL-Client/Program.cs
@@ -6,15 +6,12 @@ using OSL_Client.RiotApp.API;
 using OSL_Client.RiotApp.API.LCU;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
-using OSL_Client.RiotApp.MemoryProcessing;
+//using OSL_Client.RiotApp.MemoryProcessing;
 using OSL_Client.Communication.OSLServer;
 using System.Net;
 
 Console.WriteLine("");
 OSLLogger logger = new("Program");
-
-// Register the handler
-//CloseEvent.SetConsoleCtrlHandler(CloseEvent.Handler, true);
 
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("###########################################");
@@ -30,7 +27,7 @@ Console.ResetColor();
 
 Config.LoadConfig();
 //Initialisation for acces att the champion selection or a game
-//PostCom.Test2();
+
 String data = "Hello OSL-Server is OSL-Client managed by Sky";
 AsyncClient.StartClient(data);
 //while (!AsyncClient.StartClient(data))
@@ -47,38 +44,15 @@ while (true)
         {
             if (Config.SetHostPassGameClientApi()) //Get pass and host of client api
             {
-                //while (ProcessInfo.IdProcessInfo(Config.lockFileProcessId) != null)
-                //{
-                //    logger.log(LoggingLevel.INFO, "IdProcessInfo()", "Lol runing");
-                //    Thread.Sleep(1000);
-                //    string gameFlowPhase = ApiRequest.RequestGameClientAPI(UrlRequest.lolgameflowv1gameflowphase);
-                //    LcuApi.GameFlowPhaseCheck(gameFlowPhase);
-
-                //}
-                //Console.WriteLine("Start");
-                //TestClass.Test();
-                //Console.WriteLine("End");
-
                 string gameFlowPhase;
                 do
                 {
                     Thread.Sleep(1000);
                     gameFlowPhase = ApiRequest.RequestGameClientAPI(UrlRequest.lolgameflowv1gameflowphase); //Request game client api
                     LcuApi.GameFlowPhaseCheck(gameFlowPhase); //Check which phase
-                    //string testInformation = ApiRequest.RequestGameClientAPI("/lol-perks/v1/currentpage");
-                    //Console.WriteLine(testInformation);
                 }
                 while (gameFlowPhase != null);
             }
         }
     }
 }
-
-
-//See what phase is of the game
-//if (ApiRequest.RequestGameClientAPI(LcuApi.lolgameflowv1session))
-//while (true)
-//{
-//    logger.log(LoggingLevel.INFO, "RequestGameClientApi()", ApiRequest.RequestGameClientAPI(UrlRequest.lolgameflowv1gameflowphase));
-//    Thread.Sleep(5000);
-//}
