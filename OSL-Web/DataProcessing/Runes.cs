64656c1b22526699686b7e43f59ea33fd9da240c
Sky <fabien-perot@orange.fr>
2023-05-11 12:43:48 +0200

    OSL-Web : Runes creation list
    Correction if not data of rune in session, get it on web api riot

--- OSL-Web/DataProcessing/Runes.cs
+++ OSL-Web/DataProcessing/Runes.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using OSL_Common.System.Logging;
+using OSL_Web.Pages.Runes;
 using OSL_WebApiRiot.WebApiRiot;
 
 namespace OSL_Web.DataProcessing
@@ -11,9 +12,9 @@ namespace OSL_Web.DataProcessing
 
         public static void GetPerksInformation(string summonerName)
         {
-            dynamic jsonSummoner = JsonConvert.DeserializeObject(Summoner_V4.ByName(summonerName));
+            dynamic jsonSummoner = JsonConvert.DeserializeObject(WebApiRiot.RequestWebApiRiot(Summoner_V4.ByName(summonerName)));
             string id = jsonSummoner.id;
-            dynamic jsonSpectator = JsonConvert.DeserializeObject(Spectator_V4.BySummoner(id));
+            dynamic jsonSpectator = JsonConvert.DeserializeObject(WebApiRiot.RequestWebApiRiot(Spectator_V4.BySummoner(id)));
             CreateSummonerPerksListSpectator(jsonSpectator);
         }
 
@@ -31,34 +32,47 @@ namespace OSL_Web.DataProcessing
                     Perks = JsonConvert.DeserializeObject<Perks>(Convert.ToString(player.perks)),
                 });
             }
+            _logger.log(LoggingLevel.INFO, "CreateSummonerPerksListSpectator()", $"Player rune list created");
         }
 
         public static void CreateSummonerPerksList(string content)
         {
+
             summonerPerksList = new();
             dynamic jsonSessionInfo = JsonConvert.DeserializeObject(content);
+            try
+            {
+                foreach (var player in jsonSessionInfo.gameData.teamOne)
+                {
+                    summonerPerksList.Add(new InfoForPerks()
+                    {
+                        TeamId = 100,
+                        Lane = GetLane(Convert.ToString(player.selectedPosition)),
+                        ChampionId = player.championId,
+                        SummonerName = player.summonerName,
+                        Perks = JsonConvert.DeserializeObject<Perks>(Convert.ToString(player.gameCustomization.perks)),
+                    });
 
-            foreach (var player in jsonSessionInfo.gameData.teamOne)
-            {
-                summonerPerksList.Add(new InfoForPerks()
+                }
+                foreach (var player in jsonSessionInfo.gameData.teamTwo)
                 {
-                    TeamId = 100,
-                    Lane = GetLane(Convert.ToString(player.selectedPosition)),
-                    ChampionId = player.championId,
-                    SummonerName = player.summonerName,
-                    Perks = JsonConvert.DeserializeObject<Perks>(Convert.ToString(player.gameCustomization.perks)),
-                });
+                    summonerPerksList.Add(new InfoForPerks()
+                    {
+                        TeamId = 200,
+                        Lane = GetLane(Convert.ToString(player.selectedPosition)),
+                        ChampionId = player.championId,
+                        SummonerName = player.summonerName,
+                        Perks = JsonConvert.DeserializeObject<Perks>(Convert.ToString(player.gameCustomization.perks)),
+                    });
+                }
+                _logger.log(LoggingLevel.INFO, "CreateSummonerPerksList()", $"Player rune list created");
             }
-            foreach (var player in jsonSessionInfo.gameData.teamTwo)
+            catch (Exception e)
             {
-                summonerPerksList.Add(new InfoForPerks()
-                {
-                    TeamId = 200,
-                    Lane = GetLane(Convert.ToString(player.selectedPosition)),
-                    ChampionId = player.championId,
-                    SummonerName = player.summonerName,
-                    Perks = JsonConvert.DeserializeObject<Perks>(Convert.ToString(player.gameCustomization.perks)),
-                });
+                _logger.log(LoggingLevel.INFO, "CreateSummonerPerksList()", $"Error creation player rune list : {e.Message}");
+                string nameSummoner = jsonSessionInfo.gameData.teamOne[0].summonerName;
+                _logger.log(LoggingLevel.WARN, "CreateSummonerPerksList()", $"Try to create player rune list whith GetPerksInformation({jsonSessionInfo.gameData.teamOne[0].summonerName})");
+                GetPerksInformation(nameSummoner);
             }
         }
 
