7a2e3d76ec843c0314c8dcf90653943e72f78916
Sky <fabien-perot@orange.fr>
2023-08-15 12:24:41 +0200

    OSL-Web : change version to 1.1.5

--- OSL-Web/Program.cs
+++ OSL-Web/Program.cs
@@ -4,6 +4,7 @@ using OSL_Web.Pages.CDragon;
 using System.Diagnostics;
 using MudBlazor.Services;
 using OSL_Web.Sockets;
+using OSL_Web.Pages.InGame;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -44,31 +45,43 @@ Console.WriteLine(" \\$$    $$ \\$$    $$| $$     \\  | $$$    \\$$$ \\$$     \\
 Console.WriteLine("  \\$$$$$$   \\$$$$$$  \\$$$$$$$$   \\$$      \\$$  \\$$$$$$$ \\$$$$$$$");
 Console.WriteLine();
 Console.ForegroundColor = ConsoleColor.Red;
-Console.WriteLine(" __     __                                __                              __         ______       ______  ");
-Console.WriteLine("|  \\   |  \\                              |  \\                           _/  \\       /      \\     /      \\ ");
-Console.WriteLine("| $$   | $$  ______    ______    _______  \\$$  ______   _______        |   $$      |  $$$$$$\\   |  $$$$$$\\");
-Console.WriteLine("| $$   | $$ /      \\  /      \\  /       \\|  \\ /      \\ |       \\        \\$$$$       \\$$__| $$   | $$$\\| $$");
-Console.WriteLine(" \\$$\\ /  $$|  $$$$$$\\|  $$$$$$\\|  $$$$$$$| $$|  $$$$$$\\| $$$$$$$\\        | $$       /      $$   | $$$$\\ $$");
-Console.WriteLine("  \\$$\\  $$ | $$    $$| $$   \\$$ \\$$    \\ | $$| $$  | $$| $$  | $$        | $$      |  $$$$$$    | $$\\$$\\$$");
-Console.WriteLine("   \\$$ $$  | $$$$$$$$| $$       _\\$$$$$$\\| $$| $$__/ $$| $$  | $$       _| $$_  __ | $$_____  __| $$_\\$$$$");
-Console.WriteLine("    \\$$$    \\$$     \\| $$      |       $$| $$ \\$$    $$| $$  | $$      |   $$ \\|  \\| $$     \\|  \\\\$$  \\$$$");
-Console.WriteLine("     \\$      \\$$$$$$$ \\$$       \\$$$$$$$  \\$$  \\$$$$$$  \\$$   \\$$       \\$$$$$$ \\$$ \\$$$$$$$$ \\$$ \\$$$$$$ ");
+//Console.WriteLine(" __     __                                __                              __         ______       ______  ");
+//Console.WriteLine("|  \\   |  \\                              |  \\                           _/  \\       /      \\     /      \\ ");
+//Console.WriteLine("| $$   | $$  ______    ______    _______  \\$$  ______   _______        |   $$      |  $$$$$$\\   |  $$$$$$\\");
+//Console.WriteLine("| $$   | $$ /      \\  /      \\  /       \\|  \\ /      \\ |       \\        \\$$$$       \\$$__| $$   | $$$\\| $$");
+//Console.WriteLine(" \\$$\\ /  $$|  $$$$$$\\|  $$$$$$\\|  $$$$$$$| $$|  $$$$$$\\| $$$$$$$\\        | $$       /      $$   | $$$$\\ $$");
+//Console.WriteLine("  \\$$\\  $$ | $$    $$| $$   \\$$ \\$$    \\ | $$| $$  | $$| $$  | $$        | $$      |  $$$$$$    | $$\\$$\\$$");
+//Console.WriteLine("   \\$$ $$  | $$$$$$$$| $$       _\\$$$$$$\\| $$| $$__/ $$| $$  | $$       _| $$_  __ | $$_____  __| $$_\\$$$$");
+//Console.WriteLine("    \\$$$    \\$$     \\| $$      |       $$| $$ \\$$    $$| $$  | $$      |   $$ \\|  \\| $$     \\|  \\\\$$  \\$$$");
+//Console.WriteLine("     \\$      \\$$$$$$$ \\$$       \\$$$$$$$  \\$$  \\$$$$$$  \\$$   \\$$       \\$$$$$$ \\$$ \\$$$$$$$$ \\$$ \\$$$$$$ ");
+
+Console.WriteLine(" __     __                                __                              __          __       _______  ");
+Console.WriteLine("|  \\   |  \\                              |  \\                           _/  \\       _/  \\     |       \\ ");
+Console.WriteLine("| $$   | $$  ______    ______    _______  \\$$  ______   _______        |   $$      |   $$     | $$$$$$$ ");
+Console.WriteLine("| $$   | $$ /      \\  /      \\  /       \\|  \\ /      \\ |       \\        \\$$$$       \\$$$$     | $$____  ");
+Console.WriteLine(" \\$$\\ /  $$|  $$$$$$\\|  $$$$$$\\|  $$$$$$$| $$|  $$$$$$\\| $$$$$$$\\        | $$        | $$     | $$   \\ ");
+Console.WriteLine("  \\$$\\  $$ | $$    $$| $$   \\$$ \\$$    \\ | $$| $$  | $$| $$  | $$        | $$        | $$      \\$$$$$$$\\");
+Console.WriteLine("   \\$$ $$  | $$$$$$$$| $$       _\\$$$$$$\\| $$| $$__/ $$| $$  | $$       _| $$_  __  _| $$_  __|  \\__| $$");
+Console.WriteLine("    \\$$$    \\$$     \\| $$      |       $$| $$ \\$$    $$| $$  | $$      |   $$ \\|  \\|   $$ \\|  \\\\$$    $$");
+Console.WriteLine("     \\$      \\$$$$$$$ \\$$       \\$$$$$$$  \\$$  \\$$$$$$  \\$$   \\$$       \\$$$$$$ \\$$ \\$$$$$$ \\$$ \\$$$$$$ ");                                                                                               
 Console.ResetColor();
 Console.WriteLine();
 
 Config.LoadConfig(); //Load configs
 
-//CDragonPage.UpdateManual();//Download file fr
+CDragonPage.UpdateManual();//Download file latest fr_fr
+
+//TimerControl.InitTimerGameStart();
+Thread RunDecreasingTimer = new Thread(() => TimerControl.DecreasingTimer());
+RunDecreasingTimer.Start();
 
 AsyncServer.Run(); //Run Socket server
 
-//OSL_Web.DataProcessing.InGame.CreationGameInformation();
-
 ///Open in default browser application
-//Process.Start(new ProcessStartInfo
-//{
-//    FileName = $"http://{Config.webHostName}:{Config.webHttpPort}",
-//    UseShellExecute = true
-//});
+Process.Start(new ProcessStartInfo
+{
+    FileName = $"http://{Config.webHostName}:{Config.webHttpPort}",
+    UseShellExecute = true
+});
 
 app.Run();
