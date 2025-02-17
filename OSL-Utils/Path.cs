9b82dc59009dd51782f4c94014732e445be60bde
Sky <fabien-perot@orange.fr>
2025-02-17 20:31:21 +0100

    OSL-Utils: path management

---
+++ OSL-Utils/Path.cs
@@ -0,0 +1,11 @@
+﻿namespace OSL_Utils
+{
+    public static class Path
+    {
+        public static string Combine(params string[] paths)
+        {
+            string combinedPath = System.IO.Path.Combine(paths);
+            return combinedPath.Replace(System.IO.Path.DirectorySeparatorChar, '/');
+        }
+    }
+}
