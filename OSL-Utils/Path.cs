28f65ea9b8ef0410da55910d22af0931c6cf2845
Sky <fabien-perot@orange.fr>
2025-02-18 15:55:19 +0100

    OSL-Utils : improve path management

--- OSL-Utils/Path.cs
+++ OSL-Utils/Path.cs
@@ -1,11 +1,37 @@
 ﻿namespace OSL_Utils
 {
+    /// <summary>
+    /// Provides methods for manipulating paths.
+    /// </summary>
     public static class Path
     {
+        /// <summary>
+        /// Combines two or more strings into a path and replace \ by /.
+        /// </summary>
+        /// <param name="paths">List of path</param>
+        /// <returns>Combined path with /</returns>
         public static string Combine(params string[] paths)
         {
             string combinedPath = System.IO.Path.Combine(paths);
             return combinedPath.Replace(System.IO.Path.DirectorySeparatorChar, '/');
+        }
+
+        /// <summary>
+        /// Get the directory name of a path and replace \ by /.
+        /// </summary>
+        /// <param name="path">Path</param>
+        /// <returns>Path with /</returns>
+        public static string GetDirectoryName(string path)
+        {
+            if (path != null)
+            {
+                string? directoryName = System.IO.Path.GetDirectoryName(path);
+                if (directoryName != null)
+                {
+                    return directoryName.Replace(System.IO.Path.DirectorySeparatorChar, '/');
+                }
+            }
+            return "";
         }
     }
 }
