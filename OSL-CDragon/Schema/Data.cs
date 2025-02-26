5e16269ae55c9bc9a4a7f83aa98afe208e1ddb8f
Sky <fabien-perot@orange.fr>
2025-02-26 17:36:18 +0100

    OSL-CDragon : Add position and epicmonster in data schema

--- OSL-CDragon/Schema/Data.cs
+++ OSL-CDragon/Schema/Data.cs
@@ -9,12 +9,25 @@
         /// List of patches
         /// </summary>
         public List<Patch> Patchs { get; set; }
+
+        /// <summary>
+        /// Assets of top, jungle, mid, adc, and support positions
+        /// </summary>
+        public Position Positions { get; set; }
+
+        /// <summary>
+        /// Assets of epic monsters, dragon, baron, herald, void grubs, Atakhans
+        /// </summary>
+        public EpicMonster EpicMonsters { get; set; }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Data"/> struct.
         /// </summary>
         public Data()
         {
             Patchs = [];
+            Positions = new();
+            EpicMonsters = new();
         }
     }
 }
