c62ea35a963c9dc59ec74868d0a39c6a9b2664b8
Sky <fabien-perot@orange.fr>
2025-02-26 17:36:33 +0100

    OSL-CDragon : add dragon schema data

---
+++ OSL-CDragon/Schema/Dragon.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSL_CDragon.Schema
+{
+    /// <summary>
+    /// List of assets for each dragon
+    /// </summary>
+    internal class Dragon
+    {
+        /// <summary>
+        /// List of assets for the Cloud dragon
+        /// </summary>
+        public List<Asset> Cloud { get; set; }
+        /// <summary>
+        /// List of assets for the Chemtech dragon
+        /// </summary>
+        public List<Asset> Chemtech { get; set; }
+        /// <summary>
+        /// List of assets for the Mountain dragon
+        /// </summary>
+        public List<Asset> Mountain { get; set; }
+        /// <summary>
+        /// List of assets for the Infernal dragon
+        /// </summary>
+        public List<Asset> Infernal { get; set; }
+        /// <summary>
+        /// List of assets for the Hextech dragon
+        /// </summary>
+        public List<Asset> Hextech { get; set; }
+        /// <summary>
+        /// List of assets for the Ocean dragon
+        /// </summary>
+        public List<Asset> Ocean { get; set; }
+        /// <summary>
+        /// List of assets for the Elder dragon
+        /// </summary>
+        public List<Asset> Elder { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Dragon"/> struct.
+        /// </summary>
+        public Dragon()
+        {
+            Cloud = [];
+            Chemtech = [];
+            Mountain = [];
+            Infernal = [];
+            Hextech = [];
+            Ocean = [];
+            Elder = [];
+        }
+    }
+}
