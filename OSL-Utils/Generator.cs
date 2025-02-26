b92d73a14aeee631363d810adec3a28376bde2b5
Sky <fabien-perot@orange.fr>
2025-02-26 17:43:44 +0100

    OSL-Utils : add generator file, create GenerateSha1Id function

---
+++ OSL-Utils/Generator.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace OSL_Utils
+{
+    public static class Generator
+    {
+        public static string GenerateSha1Id()
+        {
+            string input = Guid.NewGuid().ToString() + DateTime.UtcNow.Ticks;
+            byte[] hashBytes = SHA1.HashData(Encoding.UTF8.GetBytes(input));
+            return BitConverter.ToString(hashBytes).Replace("-", "").ToLower()[..10];
+        }
+    }
+}
