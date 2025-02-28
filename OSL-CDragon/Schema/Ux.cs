70909397ec9c5b58fae63a7f4c1dc1750b4c39aa
Sky <fabien-perot@orange.fr>
2025-02-28 11:47:49 +0100

    OSL-CDragon : create Ux information

---
+++ OSL-CDragon/Schema/Ux.cs
@@ -0,0 +1,36 @@
+﻿namespace OSL_CDragon.Schema
+{
+    /// <summary>
+    /// Assets used for the user interface (UX)
+    /// </summary>
+    internal class Ux
+    {
+        /// <summary>
+        /// List of assets for runes
+        /// </summary>
+        public List<Asset> Runes { get; set; }
+        /// <summary>
+        /// List of assets for in-game
+        /// </summary>
+        public List<Asset> InGame { get; set; }
+        /// <summary>
+        /// List of assets for end-game
+        /// </summary>
+        public List<Asset> EndGame { get; set; }
+        /// <summary>
+        /// List of assets for champion select
+        /// </summary>
+        public List<Asset> ChampSelect { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Ux"/> struct.
+        /// </summary>
+        public Ux()
+        {
+            Runes = [];
+            InGame = [];
+            EndGame = [];
+            ChampSelect = [];
+        }
+    }
+}
