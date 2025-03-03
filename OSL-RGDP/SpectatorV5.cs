0d2e4b3e20f45441b645408de3015042435b526b
Sky <fabien-perot@orange.fr>
2025-03-03 16:04:09 +0100

    OSL-RGDP : add api spectator gestion

---
+++ OSL-RGDP/SpectatorV5.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using OSL_RGDP.Schema;
+using OSL_RGDP.Schema.Riot;
+using static OSL_RGDP.RgdpApi;
+
+namespace OSL_RGDP
+{
+    /// <summary>
+    /// Game information see by a spectator
+    /// </summary>
+    /// <param name="info"></param>
+    public class SpectatorV5(Info info)
+    {
+        /// <summary>
+        /// Information for make request to Riot Game Developer Portal
+        /// </summary>
+        private readonly Info _info = info;
+
+        /// <summary>
+        /// Get the current game information for the given summoner ID.
+        /// </summary>
+        /// <param name="encryptedPuuid">Encrypted puuid</param>
+        /// <returns><see cref="CurrentGameInfo"/> info</returns>
+        public CurrentGameInfo BySummoner(string encryptedPuuid)
+        {
+            string? data = Request(_info, $"/lol/spectator/v5/active-games/by-summoner/{encryptedPuuid}");
+            if (data == null)
+            {
+                return new CurrentGameInfo();
+            }
+            return JsonConvert.DeserializeObject<CurrentGameInfo>(data);
+        }
+    }
+}
