bae0055f7b44650acd9ad30fdf99065dd763fea9
Sky <github@sky-csc.fr>
2025-09-21 19:58:39 +0200

    OSl-Overlay : Champ Select handler

--- OSL-Overlay/WebSocketClient/Handlers/ChampSelectHandler.cs
+++ OSL-Overlay/WebSocketClient/Handlers/ChampSelectHandler.cs
@@ -1,6 +1,7 @@
-﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Linq;
 using OSL_Lcu.Schema.Lcu;
 using OSL_Utils;
+using OSL_Overlay.Phase.ChampSelect;
 
 namespace OSL_Overlay.WebSocketClient.Handlers
 {
@@ -14,12 +15,21 @@ namespace OSL_Overlay.WebSocketClient.Handlers
         /// <inheritdoc />
         public string Type => "champSelect";
 
+        private readonly ChampSelectState _state;
+
+        public ChampSelectHandler(ChampSelectState state)
+        {
+            _state = state;
+        }
+
         /// <inheritdoc />
         public Task HandleAsync(JToken jsonData)
         {
             var data = jsonData.ToObject<ChampSelectSession>();
             _logger.Log(LoggingLevel.INFO, "HandleAsync()", $"🎯 Champ Select management");
             // TODO : Process the ChampSelectSession data as needed
+            _state.ManageSession(data!);
+
             return Task.CompletedTask;
         }
     }
