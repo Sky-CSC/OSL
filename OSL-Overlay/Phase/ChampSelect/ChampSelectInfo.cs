01d7a98e713a4267ac883c419a1cd82eb999b12e
Sky <github@sky-csc.fr>
2025-09-21 19:57:08 +0200

    OSL-Overlay : Champ Select gestion

---
+++ OSL-Overlay/Phase/ChampSelect/ChampSelectInfo.cs
@@ -0,0 +1,228 @@
+namespace OSL_Overlay.Phase.ChampSelect
+{
+    public class ChampSelectInfo
+    {
+        public Team BlueTeam { get; set; }
+        public Team RedTeam { get; set; }
+        public string TeamsBackground { get; set; }
+        public Patch Patch { get; set; }
+        public Timer CommonTimer { get; set; }
+        public Text Phase { get; set; }
+        public Text Vs { get; set; }
+
+        public ChampSelectInfo(int nbBluePlayers, int nbRedPlayers, int nbBlueBans, int nbRedBans)
+        {
+            BlueTeam = new(nbBluePlayers, nbBlueBans);
+            RedTeam = new(nbRedPlayers, nbRedBans);
+            TeamsBackground = string.Empty;
+            Patch = new();
+            CommonTimer = new();
+            Phase = new();
+            Vs = new();
+        }
+        public ChampSelectInfo()
+        {
+            BlueTeam = new(5, 5);
+            RedTeam = new(5, 5);
+            TeamsBackground = string.Empty;
+            Patch = new();
+            CommonTimer = new();
+            Phase = new();
+            Vs = new();
+        }
+    }
+
+    public class Team
+    {
+        public string Side { get; set; }
+        public List<Ban> Bans { get; set; }
+        public Coach Coach { get; set; }
+        public Timer Timer { get; set; }
+        public List<Pick> Picks { get; set; }
+        public bool ShowLogo { get; set; }
+        public string Logo { get; set; }
+        public bool ShowName { get; set; }
+        public Text Name { get; set; }
+        public bool ShowTag { get; set; }
+        public Text Tag { get; set; }
+        public BoGraphic BoGraphic { get; set; }
+        public Team(int nbPlayers, int nbBans)
+        {
+            Side = string.Empty;
+            Bans = new(nbBans);
+            Bans = [];
+            for (int i = 0; i < nbBans; i++)
+            {
+                Bans.Add(new());
+            }
+            Coach = new();
+            Timer = new();
+            Picks = [];
+            for (int i = 0; i < nbPlayers; i++)
+            {
+                Picks.Add(new());
+            }
+            ShowLogo = false;
+            Logo = string.Empty;
+            ShowName = false;
+            Name = new();
+            ShowTag = false;
+            Tag = new();
+            BoGraphic = new();
+        }
+
+    }
+    public class Ban
+    {
+        public string BackgroundImage { get; set; }
+        public string BackgroundColor { get; set; }
+        public bool IsBanning { get; set; }
+        public string BlinkColor { get; set; }
+        public bool IsCompleted { get; set; }
+        public string CrossImage { get; set; }
+        public Ban()
+        {
+            BackgroundImage = string.Empty;
+            BackgroundColor = string.Empty;
+            IsBanning = false;
+            BlinkColor = string.Empty;
+            IsCompleted = false;
+            CrossImage = string.Empty;
+        }
+    }
+
+    public class Coach
+    {
+        public bool Show { get; set; }
+        public Text Info { get; set; }
+        public Coach()
+        {
+            Show = false;
+            Info = new();
+        }
+    }
+
+    public class Patch
+    {
+        public string BackgroundColor { get; set; }
+        public Text PatchInfo { get; set; }
+        public Text Version { get; set; }
+        public Patch()
+        {
+            BackgroundColor = string.Empty;
+            PatchInfo = new();
+            Version = new();
+        }
+    }
+
+    public class Timer
+    {
+        public bool Show { get; set; }
+        public int Duration { get; set; }
+        public string Direction { get; set; }
+        public string BarColor { get; set; }
+        public string FillColor { get; set; }
+        public Timer()
+        {
+            Show = false;
+            Duration = 0;
+            Direction = string.Empty;
+            BarColor = string.Empty;
+            FillColor = string.Empty;
+        }
+    }
+
+    public class Pick
+    {
+        public string BackgroundImage { get; set; }
+        public bool ShowLane { get; set; }
+        public string LaneImage { get; set; }
+        public bool ShowSummonerSpells { get; set; }
+        public string Spell1 { get; set; }
+        public string Spell2 { get; set; }
+        public bool IsPicking { get; set; }
+        public string BlinkColor { get; set; }
+        public Text Name { get; set; }
+        public Text Action { get; set; }
+
+        public Pick()
+        {
+            BackgroundImage = string.Empty;
+            ShowLane = true;
+            LaneImage = string.Empty;
+            ShowSummonerSpells = false;
+            Spell1 = string.Empty;
+            Spell2 = string.Empty;
+            IsPicking = false;
+            BlinkColor = string.Empty;
+            Name = new();
+            Action = new();
+        }
+    }
+
+    public class Text
+    {
+        public string Txt { get; set; }
+        public string Font { get; set; }
+        public string Color { get; set; }
+        public string Background { get; set; }
+        public string Border { get; set; }
+
+        public Text(string text, string font, string color)
+        {
+            Txt = text;
+            Font = font;
+            Color = color;
+            Background = string.Empty;
+            Border = string.Empty;
+        }
+
+        public Text(string text, string font, string color, string background, string border)
+        {
+            Txt = text;
+            Font = font;
+            Color = color;
+            Background = background;
+            Border = border;
+        }
+        public Text(string background, string border)
+        {
+            Txt = string.Empty;
+            Font = string.Empty;
+            Color = string.Empty;
+            Background = background;
+            Border = border;
+        }
+        public Text()
+        {
+            Txt = string.Empty;
+            Font = string.Empty;
+            Color = string.Empty;
+            Background = string.Empty;
+            Border = string.Empty;
+        }
+    }
+
+    public class BoGraphic
+    {
+        public bool Show { get; set; }
+        public int NbMatchForWin { get; set; }
+        public int NbWin { get; set; }
+        public Text Win { get; set; }
+        public Text Undef { get; set; }
+        public BoGraphic()
+        {
+            Show = false;
+            NbMatchForWin = 0;
+            NbWin = 0;
+            Win = new();
+            Undef = new();
+        }
+    }
+
+    public class Display
+    {
+        public bool Show { get; set; }
+        public string Info { get; set; }
+    }
+}
---
+++ OSL-Overlay/Phase/ChampSelect/ChampSelectState.cs
@@ -0,0 +1,242 @@
+using OSL_CDragon;
+using OSL_Lcu.Schema.Lcu;
+
+namespace OSL_Overlay.Phase.ChampSelect
+{
+    public class ChampSelectState
+    {
+        public ChampSelectInfo Info { get; private set; } = new();
+        public string FileStyle { get; set; } = string.Empty;
+
+        private readonly CDragon _cdragon;
+
+        public event Action? OnChange;
+
+        private ChampSelectSession _session;
+        private bool IsFirstRun { get; set; } = true;
+        public bool CustomPlayerName { get; set; } = false;
+
+        public ChampSelectState(CDragon cdragon)
+        {
+            _cdragon = cdragon;
+        }
+
+        public void ManageSession(ChampSelectSession session)
+        {
+            _session = session;
+
+            if (IsFirstRun) {
+                InitInfo(session);
+                IsFirstRun = false;
+            }
+            // Update player names
+            UpdatePlayerNames(session);
+
+            // Update champion pick
+            UpdateChampionPick(session);
+
+            // Update champion pick in progress
+            UpdateChampionPickInProgress(session);
+
+            // Update champion bans
+            UpdateChampionBans(session);
+
+            // Update champion bans in progress
+            UpdateChampionBansInProgress(session);
+
+            // Update timers phase
+            TimerPhase(session);
+
+            NotifyStateChanged();
+        }
+
+        private void InitInfo(ChampSelectSession session)
+        {
+            int nbBluePlayers = session.MyTeam.Count;
+            int nbRedPlayers = session.TheirTeam.Count;
+            int nbBlueBans = 5;
+            int nbRedBans = 5;
+
+            Info = new ChampSelectInfo(nbBluePlayers, nbRedPlayers, nbBlueBans, nbRedBans);
+
+            // TODO: Get css default information
+        }
+
+        private void UpdatePlayerNames(ChampSelectSession session)
+        {
+            if (CustomPlayerName)
+            {
+                //TODO: Get player names from config
+            }
+            else
+            {
+                foreach (var (player, index) in session.MyTeam.Select((value, idx) => (value, idx)))
+                {
+                    Info.BlueTeam.Picks[index].Name.Txt = player.GameName;
+                }
+                foreach (var (player, index) in session.TheirTeam.Select((value, idx) => (value, idx)))
+                {
+                    Info.RedTeam.Picks[index].Name.Txt = player.GameName;
+                }
+            }
+        }
+
+        private void UpdateChampionPick(ChampSelectSession session)
+        {
+            foreach (var (player, index) in session.MyTeam.Select((value, idx) => (value, idx)))
+            {
+                if (player.ChampionId != 0)
+                {
+                    Info.BlueTeam.Picks[index].BackgroundImage = _cdragon.GetChampionSplash(player.ChampionId);
+                    Info.BlueTeam.Picks[index].IsPicking = false;
+                }
+            }
+
+            foreach (var (player, index) in session.TheirTeam.Select((value, idx) => (value, idx)))
+            {
+                if (player.ChampionId != 0)
+                {
+                    Info.RedTeam.Picks[index].BackgroundImage = _cdragon.GetChampionSplash(player.ChampionId);
+                    Info.RedTeam.Picks[index].IsPicking = false;
+                }
+            }
+        }
+
+        private void UpdateChampionPickInProgress(ChampSelectSession session)
+        {
+            foreach (var contain in session.Actions)
+            {
+                foreach (var action in contain)
+                {
+                    if (action.Type == "pick" && action.IsInProgress)
+                    {
+                        if (action.IsAllyAction)
+                        {
+                            Info.BlueTeam.Picks[(int)action.ActorCellId].BackgroundImage = _cdragon.GetChampionSplash(action.ChampionId);
+                            Info.BlueTeam.Picks[(int)action.ActorCellId].IsPicking = true;
+                            UpdateTimer("blue", 25);
+                        }
+                        else if (!action.IsAllyAction)
+                        {
+                            Info.RedTeam.Picks[(int)action.ActorCellId - 5].BackgroundImage = _cdragon.GetChampionSplash(action.ChampionId);
+                            Info.RedTeam.Picks[(int)action.ActorCellId - 5].IsPicking = true;
+                            UpdateTimer("red", 25);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void UpdateChampionBans(ChampSelectSession session)
+        {
+            foreach (var (ban, index) in session.Bans.MyTeamBans.Select((value, idx) => (value, idx)))
+            {
+                Info.BlueTeam.Bans[index].BackgroundImage = _cdragon.GetChampionSquare(ban);
+                Info.BlueTeam.Bans[index].IsCompleted = true;
+                Info.BlueTeam.Bans[index].IsBanning = false;
+            }
+            foreach (var (ban, index) in session.Bans.TheirTeamBans.Select((value, idx) => (value, idx)))
+            {
+                Info.RedTeam.Bans[index].BackgroundImage = _cdragon.GetChampionSquare(ban);
+                Info.RedTeam.Bans[index].IsCompleted = true;
+                Info.RedTeam.Bans[index].IsBanning = false;
+            }
+        }
+
+        private void UpdateChampionBansInProgress(ChampSelectSession session)
+        {
+            foreach (var contain in session.Actions)
+            {
+                foreach (var action in contain)
+                {
+                    if (action.Type == "ban" && action.IsInProgress)
+                    {
+                        if (action.IsAllyAction)
+                        {
+                            Info.BlueTeam.Bans[(int)action.ActorCellId].BackgroundImage = _cdragon.GetChampionSquare(action.ChampionId);
+                            Info.BlueTeam.Bans[(int)action.ActorCellId].IsBanning = true;
+                            UpdateTimer("blue", 25);
+                        }
+                        else if (!action.IsAllyAction)
+                        {
+                            Info.RedTeam.Bans[(int)action.ActorCellId - 5].BackgroundImage = _cdragon.GetChampionSquare(action.ChampionId);
+                            Info.RedTeam.Bans[(int)action.ActorCellId - 5].IsBanning = true;
+                            UpdateTimer("red", 25);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void TimerPhase(ChampSelectSession session)
+        {
+            switch (session.Timer.Phase)
+            {
+                case "PLANNING":
+                    Info.Phase.Txt = "Planning";
+                    UpdateTimer("common", 30);
+                    break;
+                case "BAN_PICK":
+                    Info.Phase.Txt = "Ban & Pick";
+                    break;
+                case "FINALIZATION":
+                    Info.Phase.Txt = "Finalization";
+                    UpdateTimer("common", 60);
+                    break;
+                case "GAME_STARTING":
+                    Info.Phase.Txt = "Game Starting";
+                    UpdateTimer("common", 130);
+                    break;
+                default:
+                    Info.Phase.Txt = "Unknown Phase";
+                    UpdateTimer("common", 0);
+                    break;
+            }
+        }
+
+        private void UpdateTimer(string side, int duration)
+        {
+            switch (side)
+            {
+                case "blue":
+                    Info.BlueTeam.Timer.Show = true;
+                    Info.RedTeam.Timer.Show = false;
+                    Info.CommonTimer.Show = false;
+                    Info.BlueTeam.Timer.Duration = duration;
+                    break;
+                case "red":
+                    Info.RedTeam.Timer.Show = true;
+                    Info.BlueTeam.Timer.Show = true;
+                    Info.CommonTimer.Show = false;
+                    Info.RedTeam.Timer.Duration = duration;
+                    break;
+                case "common":
+                    Info.CommonTimer.Show = true;
+                    Info.BlueTeam.Timer.Show = false;
+                    Info.RedTeam.Timer.Show = false;
+                    Info.CommonTimer.Duration = duration;
+                    break;
+                default:
+                    Info.BlueTeam.Timer.Show = false;
+                    Info.RedTeam.Timer.Show = false;
+                    Info.CommonTimer.Show = false;
+                    break;
+            }
+        }
+
+        public void UpdateInfoCss(ChampSelectInfo info)
+        {
+            Info = info;
+            if (_session != null)
+                ManageSession(_session);
+            NotifyStateChanged();
+        }
+
+        public void CssChange()
+        {
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
