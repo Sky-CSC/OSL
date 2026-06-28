a7fac801fc22cd0c32dd4ddc6f6941eef8b0af95
Sky <github@sky-csc.fr>
2026-06-28 13:59:40 +0200

    OSL-Utils : add token generator

--- OSL-Utils/Generator.cs
+++ OSL-Utils/Generator.cs
@@ -1,7 +1,7 @@
-﻿using System.Text;
+using System.Text;
 using System.Security.Cryptography;
 
-namespace OSL_Utils
+namespace OSL_Utils.Generator
 {
     /// <summary>
     /// Generator class to generate unique ids
@@ -17,6 +17,16 @@ namespace OSL_Utils
             string input = Guid.NewGuid().ToString() + DateTime.UtcNow.Ticks;
             byte[] hashBytes = SHA1.HashData(Encoding.UTF8.GetBytes(input));
             return BitConverter.ToString(hashBytes).Replace("-", "").ToLower()[..10];
+        }
+
+        /// <summary>
+        /// Generate a Token 
+        /// </summary>
+        /// <returns></returns>
+        public static string GenerateToken()
+        {
+            byte[] bytes = RandomNumberGenerator.GetBytes(32);
+            return Convert.ToBase64String(bytes);
         }
     }
 }
