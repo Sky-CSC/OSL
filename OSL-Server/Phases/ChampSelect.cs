e53301971a4353096d75b5e0be0e7ef1dc85cd26
Sky <github@sky-csc.fr>
2026-06-29 23:53:07 +0200

    OSl-Server : get GameName on ChampSelect phase

--- OSL-Server/Phases/ChampSelect.cs
+++ OSL-Server/Phases/ChampSelect.cs
@@ -1,4 +1,5 @@
 using OSL_Lcu;
+using OSL_Lcu.Schema.Lcu;
 using OSL_Utils;
 
 namespace OSL_Server.Phases
@@ -10,7 +11,7 @@ namespace OSL_Server.Phases
     /// <param name="wsServer">Socket server</param>
     public class ChampSelect(LcuEndpoints lcuEndpoints, WebSocketServer.WebSocketServer wsServer)
     {
-        private static readonly Logger _logger = new("GameFlow");
+        private static readonly Logger _logger = new("ChampSelect");
 
         private readonly LcuEndpoints _lcuEndpoints = lcuEndpoints;
         private readonly WebSocketServer.WebSocketServer _wsServer = wsServer;
@@ -31,12 +32,47 @@ namespace OSL_Server.Phases
                 }
                 _logger.Log(LoggingLevel.INFO, "SyncChampSelectAsync()", $"📊 Champ Select : {gameflowPhase}");
                 // Send champ select details to the WebSocket server
+                gameflowPhase = GameName(gameflowPhase);
                 await _wsServer.SetChampSelectAsync(gameflowPhase);
             }
             catch (Exception ex)
             {
                 _logger.Log(LoggingLevel.ERROR, "SyncChampSelectAsync()", $"❌ {ex.Message}");
             }
+        }
+
+        /// <summary>
+        /// Retrieves the game names of players in the champion selection phase and updates the ChampSelectSession object accordingly.
+        /// </summary>
+        /// <param name="gameflowPhase"></param>
+        /// <returns></returns>
+        public ChampSelectSession GameName(ChampSelectSession gameflowPhase)
+        {
+            gameflowPhase.MyTeam = CheckTeamsSummonerNames(gameflowPhase.MyTeam);
+            gameflowPhase.TheirTeam = CheckTeamsSummonerNames(gameflowPhase.TheirTeam);
+            return gameflowPhase;
+        }
+
+        /// <summary>
+        /// Checks the summoner names of players in a team and updates their GameName property if it is null or empty.
+        /// </summary>
+        /// <param name="team"></param>
+        /// <returns></returns>
+        public List<ChampSelectPlayerSelection> CheckTeamsSummonerNames(List<ChampSelectPlayerSelection> team)
+        {
+            foreach (var player in team)
+            {
+                if (!String.IsNullOrEmpty(player.GameName))
+                {
+                    continue;
+                }
+                var summoner = _lcuEndpoints.GetSummonerByIdAsync(player.SummonerId).Result;
+                if (summoner != null)
+                {
+                    player.GameName = summoner.GameName;
+                }
+            }
+            return team;
         }
     }
 }
